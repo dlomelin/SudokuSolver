96d7281c9106deac8b241da92a2ec9c35a6c6ad8
David Lomelin <david.lomelin@gmail.com>
2014-05-28 23:59:43 -0700

    Minor comments

--- modules/OptionParser.py
+++ modules/OptionParser.py
@@ -1,6 +1,10 @@
 from optparse import OptionParser
 
+# Add functionality to the OptionParser module
 class OptionParser(OptionParser):
+
+	# Make sure the specified parameter was supplied by the user
+	# Ex: self.check_required('--file')
 	def check_required(self, param):
 		option = self.get_option(param)
 		if getattr(self.values, option.dest) is None:
---
+++ modules/t/test_SudokuBlock.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestSudokuBlock(unittest.TestCase):
+	def setUp(self):
+		pass
+
+	def test_valid(self):
+		pass
+
+if __name__ == '__main__':
+	unittest.main()
