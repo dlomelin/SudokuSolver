15c56b6bb209e210bfae14fd2236e9035da87036
David Lomelin <david.lomelin@gmail.com>
2014-06-03 17:29:16 -0700

    Overloaded string and == operators

--- modules/SudokuCoordinates.py
+++ modules/SudokuCoordinates.py
@@ -5,3 +5,13 @@ class SudokuCoordinates(object):
 		self.blockCol = blockCol
 		self.row = row
 		self.col = col
+
+	def __str__(self):
+		return '(%s,%s,%s,%s)' % (self.blockRow, self.blockCol, self.row, self.col)
+
+	def __eq__(self, other):
+		if self.blockRow == other.blockRow and self.blockCol == other.blockCol and \
+			self.row == other.row and self.col == other.col:
+			return True
+		else:
+			return False
