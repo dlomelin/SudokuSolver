602ebf43ba1a5881a7d14c4e12c546e4f12d1bf1
David Lomelin <david.lomelin@gmail.com>
2014-06-09 00:03:43 -0700

    Added functionality to allow storing SudokuCoordinates objects in a set

--- modules/SudokuCoordinates.py
+++ modules/SudokuCoordinates.py
@@ -6,15 +6,25 @@ class SudokuCoordinates(object):
 		self.row = row
 		self.col = col
 
+	# Just for thoroughness
+	def __repr__(self):
+		return '%s(%s,%s,%s,%s)' % (self.__class__.__name__, self.blockRow, self.blockCol, self.row, self.col)
+
+	# Allows for storing object in a set([]) and using a sets' methods correctly
+	def __hash__(self):
+		return hash((self.blockRow, self.blockCol, self.row, self.col))
+
+	# Allows for using str(obj)
 	def __str__(self):
 		return '(%s,%s,%s,%s)' % (self.blockRow, self.blockCol, self.row, self.col)
 
+	# Allows for testing obj1 == obj2
 	def __eq__(self, other):
-		if self.blockRow == other.blockRow and self.blockCol == other.blockCol and \
-			self.row == other.row and self.col == other.col:
-			return True
-		else:
-			return False
+		return isinstance(other, self.__class__) and self.alignsByRow(other) and self.alignsByCol(other)
+
+	# Allows for testing obj1 != obj2
+	def __ne__(self, other):
+		return not self.__eq__(other)
 
 	##################
 	# Public Methosd #
@@ -37,3 +47,4 @@ class SudokuCoordinates(object):
 			return True
 		else:
 			return False
+
